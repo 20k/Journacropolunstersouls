20fb8242c6c5794a1f9320e2b569c1258437bc0e
20k <the_berronator@hotmail.co.uk>
2016-06-30 10:54:46 +0100

    Proper spawn in. Need to make it maximally compact, also rotate cube wall

--- Assets/MagicSystem.cs
+++ Assets/MagicSystem.cs
@@ -8,18 +8,70 @@ public class MagicSystem : MonoBehaviour {
     GameObject iceCube;
     int placed = 0;
 
+    Vector3 firstPos = new Vector3(0, 0, 0);
+
 	// Use this for initialization
 	void Start () {
         iceCube = GameObject.Find("IceCube");
     }
 	
+    void Spawn(Vector3 pos)
+    {
+        GameObject obj = Instantiate(iceCube, pos, Quaternion.identity) as GameObject;
+
+        obj.SetActive(true);
+    }
+
+    void SpawnBetween(Vector3 p1, Vector3 p2, float hsep, float vsep, int height)
+    {
+        //float size = 3;
+
+        float dist = (p2 - p1).magnitude;
+
+        float num = Mathf.Floor(dist / hsep);
+
+        //float spacing = dist / num;
+
+        Vector3 cur = p1;
+        Vector3 dir = (p2 - p1) / num;
+
+        for(int i=0; i<(int)num; i++)
+        {
+            Vector3 hpos = cur;
+
+            for(int h=0; h<height; h++)
+            {
+                Spawn(hpos);
+
+                hpos.y += vsep;
+            }
+
+            cur = cur + dir;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown("q"))
-        {
-            GameObject obj = Instantiate(iceCube, position.position, Quaternion.identity) as GameObject;
+        bool q = Input.GetKeyDown("q");
 
-            obj.SetActive(true);
+        if (q && placed == 0)
+        {
+            firstPos = position.position;
+
+            placed = 1;
+
+            return;
         }
+
+        if (q && placed == 1)
+        {
+            ///+ front distance offset
+            SpawnBetween(firstPos, position.position, 2, 3, 5);
+
+            placed = 0;
+
+            return;
+        }
+
     }
 }
