69a3257dceb16d4fa4d96625c3b726e8e152d971
20k <the_berronator@hotmail.co.uk>
2016-06-30 19:34:52 +0100

    status effecting

---
+++ Assets/IceWall.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusEffect : MonoBehaviour
+{
+    public string activateTag;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    /// <summary>
+    /// won't be a wiggles master that enters because its just a collider
+    /// </summary>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != activateTag)
+            return;
+
+        Debug.Log("Something we want entered");
+
+        StatusEffectManager manage = other.gameObject.GetComponentInChildren<StatusEffectManager>();
+
+        if (manage == null)
+            return;
+
+        manage.Stun();
+
+        gameObject.SetActive(false);
+
+        /*WigglesMaster wg = other.gameObject.GetComponent<WigglesMaster>();
+
+        if (wg == null)
+            return;
+
+        Debug.Log("wiggles enter");*/
+    }
+}
+
+
+
+/// <summary>
+/// rename me to icewall, magicsystem is a level up from here
+/// EnemyDamaging is the tag that can be affected by IceWall
+/// </summary>
+public class IceWall : MonoBehaviour {
+
+    public Transform position;
+    public string activateTag = "EnemyDamaging"; //bossmonster type
+
+    GameObject iceCube;
+    int placed = 0;
+
+    Vector3 firstPos = new Vector3(0, 0, 0);
+
+    //BoxCollider col;
+
+	// Use this for initialization
+	void Start () {
+        iceCube = GameObject.Find("IceCube");
+
+        /*col = gameObject.AddComponent<BoxCollider>();
+
+        col.size = new Vector3(0, 0, 0);
+        col.isTrigger = true;
+        col.enabled = false;*/
+    }
+
+    Quaternion GetQuat(Vector3 p1, Vector3 p2)
+    {
+        Vector3 diff = (p2 - p1);
+
+        float yangle = -Mathf.Atan2(diff.z, diff.x);
+
+        return Quaternion.Euler(0, yangle * Mathf.Rad2Deg, 0);
+    }
+	
+    void Spawn(Vector3 pos, float yangle)
+    {
+        float mangle = yangle * Mathf.Rad2Deg;
+
+        Quaternion quat;
+        quat = Quaternion.Euler(0, mangle, 0);
+
+        GameObject obj = Instantiate(iceCube, pos, quat) as GameObject;
+
+        obj.SetActive(true);
+    }
+
+    void SpawnBetween(Vector3 p1, Vector3 p2, float hsep, float vsep, int height)
+    {
+        float dist = (p2 - p1).magnitude;
+
+        float num = Mathf.Floor(dist / hsep);
+
+        Vector3 cur = p1;
+        Vector3 dir = (p2 - p1) / num;
+
+        ///cubes are symmetric, so who cares?
+        float yangle = -Mathf.Atan2(dir.z, dir.x);
+
+        for(int i=0; i<(int)num; i++)
+        {
+            Vector3 hpos = cur;
+
+            for(int h=0; h<height; h++)
+            {
+                Spawn(hpos, yangle);
+
+                hpos.y += vsep;
+            }
+
+            cur = cur + dir;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        bool q = Input.GetKeyDown("q");
+
+        if (q && placed == 0)
+        {
+            firstPos = position.position;
+
+            placed = 1;
+
+            return;
+        }
+
+        if (q && placed == 1)
+        {
+            ///initial separation
+            float heightSep = 3;
+            ///total height in cubes
+            int heightNum = 5;
+
+            float awidth = iceCube.transform.localScale.x * 1.5f;
+
+            float height = iceCube.transform.localScale.y * 1.5f * heightNum;
+
+            Vector3 localPos = new Vector3(-awidth / 2f, height / 2 - awidth / 2f, 0);
+
+            GameObject gobj = new GameObject();
+
+            StatusEffect effect = gobj.AddComponent<StatusEffect>();
+            effect.activateTag = activateTag;
+
+            gobj.SetActive(true);
+            gobj.transform.position = (firstPos + position.position) / 2f + GetQuat(firstPos, position.position) * localPos;
+
+            BoxCollider col = gobj.AddComponent<BoxCollider>();
+            col.isTrigger = true;
+
+            col.size = new Vector3((firstPos - position.position).magnitude, height, awidth);
+
+            col.transform.rotation = GetQuat(firstPos, position.position);
+            col.enabled = true;
+
+            ///+ front distance offset
+            SpawnBetween(firstPos, position.position, awidth, heightSep, heightNum);
+
+            placed = 0;
+
+            return;
+        }
+    }
+
+}
---
+++ Assets/StatusEffectManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class StatusEffectManager : MonoBehaviour {
+
+    public float stunTimeSeconds = 8;
+    public int numberOfCycles = 8;
+    public float orbitRad = 2;
+    public Transform stunOrbit;
+
+    float stunTimeRemaining = 0;
+    GameObject stunCube;
+
+    GameObject[] myStunCubes = new GameObject[2];
+
+	// Use this for initialization
+	void Start () {
+        stunCube = GameObject.Find("StunCube");
+
+        GameObject o1 = Instantiate(stunCube, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+        GameObject o2 = Instantiate(stunCube, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+
+        myStunCubes[0] = o1;
+        myStunCubes[1] = o2;
+
+        Stun();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ConditionalActivate();
+
+        if (!IsStunned())
+            return;
+
+        float timePerCycle = stunTimeSeconds / numberOfCycles;
+
+        float angle = (stunTimeRemaining / timePerCycle) * 2f * Mathf.PI;
+
+        float xp = Mathf.Cos(angle) * orbitRad;
+        float yp = Mathf.Sin(angle) * orbitRad;
+
+        Vector3 offset = new Vector3(xp, 0, yp);
+
+        myStunCubes[0].transform.position = offset + stunOrbit.transform.position;
+        myStunCubes[1].transform.position = -offset + stunOrbit.transform.position;
+
+        stunTimeRemaining -= Time.deltaTime;
+
+        if (stunTimeRemaining < 0)
+            stunTimeRemaining = 0;
+	}
+
+    public void Stun()
+    {
+        stunTimeRemaining += stunTimeSeconds;
+    }
+
+    public bool IsStunned()
+    {
+        return stunTimeRemaining > 0;
+    }
+
+    void ConditionalActivate()
+    {
+        for(int i=0; i<myStunCubes.Length; i++)
+        {
+            myStunCubes[i].SetActive(IsStunned());
+        }
+    }
+}
