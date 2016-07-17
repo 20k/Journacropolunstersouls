265d85311767e86107b5c5a3635832713dbd8ee9
20k <the_berronator@hotmail.co.uk>
2016-07-18 00:33:04 +0100

    Boss physics ragdolliser, needs to have a generic component supplied to disable instead of just wiggles, but this is pretty ez in the long term

---
+++ Assets/PhysicsRagdolliser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsRagdolliser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.F1))
+        {
+            ToRagdoll();
+
+            WigglesMaster master = GetComponent<WigglesMaster>();
+
+            master.enabled = false;
+        }
+	}
+
+    void ToRagdoll()
+    {
+        Damageable[] toRagdoll = GetComponentsInChildren<Damageable>();
+        
+        for(int i=0; i<toRagdoll.Length; i++)
+        {
+            GameObject obj = toRagdoll[i].gameObject;
+
+            obj.AddComponent<Rigidbody>();
+        }
+    }
+}
