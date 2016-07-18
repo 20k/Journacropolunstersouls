d5b8cc952600050c4451c09d8eef38f6b4c47273
20k <the_berronator@hotmail.co.uk>
2016-07-18 01:06:10 +0100

    doesn't really change much, other than deplete the worst abuse

---
+++ Assets/Depenetrator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Depenetrator : MonoBehaviour {
+
+    public float maxVel = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody body = GetComponent<Rigidbody>();
+
+        body.maxDepenetrationVelocity = maxVel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
